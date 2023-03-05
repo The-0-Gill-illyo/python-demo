b22cdf67cb6e1282f4a4e78310dc43f48b60e77d
The-0-Gill-illyo <theogill1369@gmail.com>
2023-03-05 13:34:46 -0500

    Turned if/else statements into a while loop

--- main.py
+++ main.py
@@ -3,13 +3,18 @@ print(greeting)
 
 fav_animal = input("What is your favorite animal? ")
 
-# print(fav_animal)
+valid_response = False
 
-if fav_animal == "Zombie":
-    print("I asked about an animal, but I'm glad you're ready for the apocalypse!")
-elif fav_animal == "Fish":
-    print("I can tell you spend a lot of time in the water :)")
-elif fav_animal == "Turkey":
-    print("And what does the turkey feel about Thanksgiving?")
-else:
-    print("I don't think you know what an animal is, sorry!")
+while valid_response == False:
+    
+    if fav_animal == "Zombie":
+        print("I asked about an animal, but I'm glad you're ready for the apocalypse!")
+        valid_response = True
+    elif fav_animal == "Fish":
+        print("I can tell you spend a lot of time in the water :)")
+        valid_response = True
+    elif fav_animal == "Turkey":
+        print("And what does the turkey feel about Thanksgiving?")
+        valid_response = True
+    else:
+        print("I don't think you know what an animal is, sorry!")
