624c1b88ab6df27b361b2647099e96e0d9af6974
The-0-Gill-illyo <theogill1369@gmail.com>
2023-03-05 13:25:23 -0500

    Just playing around if some if/else statements

---
+++ main.py
@@ -0,0 +1,15 @@
+greeting = "Hello world!"
+print(greeting)
+
+fav_animal = input("What is your favorite animal? ")
+
+# print(fav_animal)
+
+if fav_animal == "Zombie":
+    print("I asked about an animal, but I'm glad you're ready for the apocalypse!")
+elif fav_animal == "Fish":
+    print("I can tell you spend a lot of time in the water :)")
+elif fav_animal == "Turkey":
+    print("And what does the turkey feel about Thanksgiving?")
+else:
+    print("I don't think you know what an animal is, sorry!")
